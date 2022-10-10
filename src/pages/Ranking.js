a2242441a071355eeb65e7f016e0a8ef5c7e06e6
Alvim-JP <alvimjp12@gmail.com>
2022-10-10 20:22:24 -0300

    [Grupo 15] Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <p
@@ -10,8 +12,21 @@ export default class Ranking extends React.Component {
           Ranking
 
         </p>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Iniciar Novamente
+        </button>
       </div>
 
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
